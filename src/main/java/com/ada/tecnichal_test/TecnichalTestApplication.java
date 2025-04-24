ba7f90fb50fcc07ad9baa3447f4e8b3abac90ba4
andres.ganan <andres.ganan@mantum.com.co>
2025-04-24 10:30:20 -0500

    add: se agrega primer commot con la estructura del proyecto

---
+++ src/main/java/com/ada/tecnichal_test/TecnichalTestApplication.java
@@ -0,0 +1,13 @@
+package com.ada.tecnichal_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnichalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnichalTestApplication.class, args);
+	}
+
+}
